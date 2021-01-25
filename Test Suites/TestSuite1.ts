c246ad4f6f4eda024128c2c6ea404d6652cfd8e4
garinyr <garinyr92@gmail.com>
2021-01-25 19:16:47 +0700

    test message

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>98248b10-f6f3-4aa7-9021-7e86907a5b27</testSuiteGuid>
+   <testCaseLink>
+      <guid>c606b99d-9b34-4a0e-bb1b-03a77118c2cd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
