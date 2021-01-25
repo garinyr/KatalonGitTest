8428e637e8e414977bb12c0f7c6649bff8b2e6c4
garinyr <garinyr92@gmail.com>
2021-01-25 21:09:34 +0700

    bug fix rev1

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7b19e6ad-5299-418d-8ced-2e6a90f07859</testSuiteGuid>
+   <testCaseLink>
+      <guid>1f5c1058-f224-44ae-8709-19fe28a9622f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fe18ba07-ba08-495a-92c8-24a9a97caab8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
